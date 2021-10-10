8fe9cb5b10308a32a9a1642a5259bde38217e1f5
abdul-latif amadu <amadu3003@gmail.com>
2021-10-10 20:44:21 +0100

    Home page created
    files of components that make up home page have been created

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <div>
+      <h1 class="text-center">Pokemon</h1>
+    </div>
+    <div>
+      <Navbar />
+    </div>
+    <div>
+      <FilterInputs />
+    </div>
+    <div>
+      <Items />
+    </div>
+    <div>
+      <Navbar />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+import Items from '../components/Items.vue';
+import FilterInputs from '../components/FilterInputs.vue';
+
+export default {
+  components: {
+    Navbar,
+    Items,
+    FilterInputs,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
